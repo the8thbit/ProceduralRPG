6ae5d5d01932da9b8f421119106d83e934b6993d
nikitapond <nikita.pond@icloud.com>
2020-04-21 19:26:39 +0100

    Test commit

--- Assets/Scripts/Test/TestMain.cs
+++ Assets/Scripts/Test/TestMain.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-
+/// <summary>
+/// Adding documentation to test how git works
+/// </summary>
 public class TestMain : MonoBehaviour
 {
     public static EventManager EventManager;
